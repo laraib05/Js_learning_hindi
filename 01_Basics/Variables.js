97f1c5121b433051e5a059137712ba61d6d242e8
laraib khan <131883578+laraib05@users.noreply.github.com>
2025-01-18 22:02:05 +0000

    var

---
+++ 01_Basics/Variables.js
@@ -0,0 +1,14 @@
+const userId =23;
+// userId = 24; we can't redeclare or reinitialise
+console.log(userId);
+
+let password = 2345;
+// let password = 3555; we cant redeclare
+password =34 //but we can reinitalise
+
+
+var a = 345;
+var a = 344;// we can redeclare and reinitialise 
+
+console.log([userId,password,a]);
+console.log(userId,password,a);
